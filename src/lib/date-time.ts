94d7d2afb44ad140a86af13d2ccadb337416cd03
Gerald <58556422+geraldseville@users.noreply.github.com>
2026-08-08 01:28:16 +0800

    feat(datetime): add timezone validation utility

--- src/lib/date-time.ts
+++ src/lib/date-time.ts
@@ -1,3 +1,15 @@
 export const timezones = Intl.supportedValuesOf('timeZone');
 
 export const defaultTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+export const isValidTimezone = (timezone: string) => {
+  try {
+    Intl.DateTimeFormat(undefined, {
+      timeZone: timezone,
+    });
+
+    return true;
+  } catch {
+    return false;
+  }
+};
