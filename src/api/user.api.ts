ac0ecbab9c23340ab286220d0b55950c23137e78
Gerald <58556422+geraldseville@users.noreply.github.com>
2026-08-09 03:50:42 +0800

    refactor(users): use typed API responses

--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -1,31 +1,28 @@
-import type { Response } from '@/api/api';
 import { apiClient } from '@/api/api';
 
-import type { UserChangePasswordDto, UserDto } from '@/types/user.types';
+import type { User, UserChangePasswordDto, UserDto } from '@/types/user.types';
 
-export const changeMyPassword = (
-  payload: UserChangePasswordDto,
-): Promise<Response> => {
-  return apiClient('/users/me/change-password', {
+export const changeMyPassword = (payload: UserChangePasswordDto) => {
+  return apiClient<{ user: User }>('/users/me/change-password', {
     method: 'PATCH',
     body: JSON.stringify(payload),
   });
 };
 
-export const deleteMyAccount = (): Promise<Response> => {
-  return apiClient('/users/me', {
+export const deleteMyAccount = () => {
+  return apiClient<{ user: User }>('/users/me', {
     method: 'DELETE',
   });
 };
 
-export const getUsers = (): Promise<Response> => {
-  return apiClient('/users', {
+export const getUsers = () => {
+  return apiClient<{ users: User[] }>('/users', {
     method: 'GET',
   });
 };
 
-export const updateMe = (payload: UserDto): Promise<Response> => {
-  return apiClient('/users/me', {
+export const updateMe = (payload: UserDto) => {
+  return apiClient<{ user: User }>('/users/me', {
     method: 'PATCH',
     body: JSON.stringify(payload),
   });
