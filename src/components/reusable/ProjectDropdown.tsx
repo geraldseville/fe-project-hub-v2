7efe66867c9fdb5a4bd103e16bf70e9cf4ed0ea6
Gerald <58556422+geraldseville@users.noreply.github.com>
2026-08-11 00:28:55 +0800

    feat(project): add floating project dropdown

---
+++ src/components/reusable/ProjectDropdown.tsx
@@ -0,0 +1,123 @@
+import { useCallback, useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+import {
+  autoUpdate,
+  flip,
+  FloatingPortal,
+  offset,
+  shift,
+  useClick,
+  useDismiss,
+  useFloating,
+  useInteractions,
+  useRole,
+} from '@floating-ui/react';
+import clsx from 'clsx';
+
+import { PROJECT_STATUS_COLORS } from '@/utils/project.utils';
+
+import type { Project } from '@/types/project.types';
+
+import ProjectAsTag from '@/components/reusable/ProjectAsTag';
+
+interface ProjectDropdownProps {
+  projects: Project[];
+}
+
+export default function ProjectDropdown({ projects }: ProjectDropdownProps) {
+  const router = useRouter();
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const { refs, floatingStyles, context } = useFloating({
+    open: isOpen,
+    onOpenChange: setIsOpen,
+    placement: 'bottom-start',
+    whileElementsMounted: autoUpdate,
+    middleware: [offset(8), flip(), shift({ padding: 8 })],
+  });
+
+  const click = useClick(context);
+  const dismiss = useDismiss(context);
+  const role = useRole(context, {
+    role: 'dialog',
+  });
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([
+    click,
+    dismiss,
+    role,
+  ]);
+
+  const setReferenceRef = useCallback(
+    (node: HTMLButtonElement | null) => {
+      refs.setReference(node);
+    },
+    [refs],
+  );
+
+  const setFloatingRef = useCallback(
+    (node: HTMLDivElement | null) => {
+      refs.setFloating(node);
+    },
+    [refs],
+  );
+
+  return (
+    <>
+      <button
+        className="relative"
+        ref={setReferenceRef}
+        type="button"
+        {...getReferenceProps()}
+      >
+        <ProjectAsTag
+          title={projects[0].title}
+          color={PROJECT_STATUS_COLORS[projects[0].status].hex ?? '#000000'}
+        />
+      </button>
+
+      {isOpen && (
+        <FloatingPortal>
+          <div
+            className={clsx(
+              'z-50',
+              'w-[240px] max-h-[280px]',
+              'overflow-y-auto',
+              'p-2',
+              'rounded-lg',
+              'bg-[#131B2E]',
+              'border border-[#464554]',
+              'shadow-xl',
+            )}
+            ref={setFloatingRef}
+            style={floatingStyles}
+            {...getFloatingProps()}
+          >
+            <div className="flex flex-wrap gap-1.5">
+              {projects.map((project) => (
+                <button
+                  className=""
+                  key={project.id}
+                  type="button"
+                  onClick={() => {
+                    router.replace(`/projects/${project.id}`);
+                  }}
+                >
+                  {/* Project tag goes here */}
+                  <ProjectAsTag
+                    title={project.title}
+                    color={
+                      PROJECT_STATUS_COLORS[project.status].hex ?? '#000000'
+                    }
+                  />
+                </button>
+              ))}
+            </div>
+          </div>
+        </FloatingPortal>
+      )}
+    </>
+  );
+}
