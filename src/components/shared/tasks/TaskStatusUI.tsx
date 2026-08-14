bb7990ffb34001b622b2134ceea73f6f2cf85d42
Gerald <58556422+geraldseville@users.noreply.github.com>
2026-08-14 21:40:34 +0800

    feat: add task status UI component

---
+++ src/components/shared/tasks/TaskStatusUI.tsx
@@ -0,0 +1,36 @@
+import clsx from 'clsx';
+
+import { TASK_STATUS_COLORS } from '@/utils/task.utils';
+
+import type { TaskStatus } from '@/types/task.types';
+
+interface TaskStatusProps {
+  status: TaskStatus;
+}
+
+export default function TaskStatusUI({ status }: TaskStatusProps) {
+  const styles = TASK_STATUS_COLORS[status];
+
+  return (
+    <div
+      className={clsx(
+        'flex justify-center items-center',
+        'w-fit h-6',
+        'py-1 px-2',
+        'rounded-full',
+        styles.bg,
+        styles.border,
+      )}
+    >
+      <div
+        className={clsx(
+          'font-inter font-medium',
+          'text-[12px] leading-none',
+          styles.text,
+        )}
+      >
+        {status.replace('_', ' ')}
+      </div>
+    </div>
+  );
+}
