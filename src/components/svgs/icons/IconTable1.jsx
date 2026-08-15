3f19ce6aeae1a46a437cc7398a44d6894d1b642a
Gerald <58556422+geraldseville@users.noreply.github.com>
2026-08-15 11:51:48 +0800

    feat(svgs): added new svgs icons as reusable components

---
+++ src/components/svgs/icons/IconTable1.jsx
@@ -0,0 +1,18 @@
+export default function IconTable1({ className = '' }) {
+  return (
+    <svg
+      className={className}
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        className="path-01"
+        d="M10.8 0H1.2C0.54 0 0 0.6 0 1.33333V10.6667C0 11.4 0.54 12 1.2 12H10.8C11.46 12 12 11.4 12 10.6667V1.33333C12 0.6 11.46 0 10.8 0ZM3.6 6.66667H1.2V4H3.6V6.66667ZM4.8 4H7.2V6.66667H4.8V4ZM8.4 4H10.8V6.66667H8.4V4ZM1.2 10.6667V8H3.6V10.6667H1.2ZM4.8 10.6667V8H7.2V10.6667H4.8ZM8.4 10.6667V8H10.8V10.6667H8.4Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
