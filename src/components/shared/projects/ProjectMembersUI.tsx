02dbbb8519d17d17fc2d7c09456b3e3caf145dcd
Gerald <58556422+geraldseville@users.noreply.github.com>
2026-08-19 00:32:23 +0800

    refactor(ProjectMembersUI): add customization options and maxDisplay support

--- src/components/shared/projects/ProjectMembersUI.tsx
+++ src/components/shared/projects/ProjectMembersUI.tsx
@@ -7,16 +7,27 @@ import type { User } from '@/types/user.types';
 import ImageStack from '@/components/elements/ImageStack';
 
 interface ProjectMembersUIProps {
+  classNames?: {
+    root?: string;
+    item?: string;
+    empty?: string;
+  };
   members: User[];
+  maxDisplay?: number;
 }
 
-export default function ProjectMembersUI({ members }: ProjectMembersUIProps) {
+export default function ProjectMembersUI({
+  classNames,
+  members,
+  maxDisplay,
+}: ProjectMembersUIProps) {
   if (members.length === 0) {
     return (
       <div
         className={clsx(
           'font-hanken-grotesk',
           'text-[#DAE2FD] leading-tight truncate',
+          classNames?.empty,
         )}
       >
         No Members
@@ -26,12 +37,16 @@ export default function ProjectMembersUI({ members }: ProjectMembersUIProps) {
 
   return (
     <ImageStack
-      className={{ item: 'border-[#131B2E]!' }}
+      className={{
+        root: classNames?.root,
+        item: clsx('border-[#131B2E]!', classNames?.item),
+      }}
       images={members.map((item) => ({
         src: item.imageUrl ?? '',
         alt: getFullName(item.firstName, item.lastName),
       }))}
       size={32}
+      maxDisplay={maxDisplay}
     />
   );
 }
