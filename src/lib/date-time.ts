af62f3275e50dc219096fc97b085d27974606e59
Gerald <58556422+geraldseville@users.noreply.github.com>
2026-08-18 11:33:15 +0800

    feat(dateTime): add default time format constant and import TimeFormat type

--- src/lib/date-time.ts
+++ src/lib/date-time.ts
@@ -1,7 +1,11 @@
+import { TimeFormat } from '@/types/user.types';
+
 export const TIMEZONES = Intl.supportedValuesOf('timeZone');
 
 export const DEFAULT_TIMEZONE =
   Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+export const DEFAULT_TIME_FORMAT: TimeFormat = 'H12';
 
 export const isValidTimezone = (timezone: string) => {
   try {
