81dec643c09ec35c1e8a985b4d4f5b1796a28bda
Gerald <58556422+geraldseville@users.noreply.github.com>
2026-08-19 00:32:01 +0800

    refactor(TeamPage): rename component and integrate AppShellHead layout

--- src/app/(protected)/teams/[id]/page.tsx
+++ src/app/(protected)/teams/[id]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useMemo, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
@@ -9,28 +10,29 @@ import momentTimezone from 'moment-timezone';
 
 import { useUser } from '@/hooks/queries/useUser';
 
+import { DEFAULT_TIMEZONE, readableTimezone } from '@/utils/date-time';
+import { PROJECT_DEFAULT_COLOR } from '@/utils/project.utils';
 import { getFullName } from '@/utils/user.utils';
 
+import AppShellHead from '@/components/AppShellHead';
 import CopyToClipboard from '@/components/elements/CopyToClipboard';
+import SingleSelect from '@/components/elements/SingleSelect';
 import SkeletonLoading from '@/components/elements/SkeletonLoading';
 import Avatar from '@/components/reusable/Avatar';
 import ProjectDescriptionUI from '@/components/shared/projects/ProjectDescriptionUI';
 import ProjectMembersUI from '@/components/shared/projects/ProjectMembersUI';
+import ProjectPriorityUI from '@/components/shared/projects/ProjectPriorityUI';
 import ProjectStatusUI from '@/components/shared/projects/ProjectStatusUI';
 import ProjectTaskCounterUI from '@/components/shared/projects/ProjectTaskCounterUI';
 import ProjectTitleUI from '@/components/shared/projects/ProjectTitleUI';
-import ProjectUrgencyUI from '@/components/shared/projects/ProjectUrgencyUI';
 import {
-  IconAngleRight,
   IconCalendar2,
   IconDot,
   IconFolder1,
   IconProfileDetails,
 } from '@/components/svgs/icons';
 
-import { defaultTimezone, readableTimezone } from '@/lib/date-time';
-
-export default function TeamItemPage() {
+export default function TeamPage() {
   const params = useParams();
 
   const userId = params.id as string;
@@ -39,57 +41,34 @@ export default function TeamItemPage() {
 
   const userFullName = getFullName(user?.firstName, user?.lastName) || '';
 
-  const timezone = user?.timezone ?? defaultTimezone;
+  const timezone = user?.timezone ?? DEFAULT_TIMEZONE;
 
   const joinedDate = momentTimezone(user?.createdAt)
     .tz(timezone)
     .format('MMM DD, YYYY');
 
-  console.log({ userId, user, joinedDate });
+  const [myProjectType, setMyProjectType] = useState<'Owned' | 'Assigned'>(
+    'Owned',
+  );
+
+  const visibleProjects = useMemo(
+    () =>
+      myProjectType === 'Owned'
+        ? (user?.ownedProjects ?? [])
+        : (user?.memberProjects ?? []),
+    [myProjectType, user],
+  );
 
   return (
-    <main className={clsx('overflow-auto', 'w-full h-screen', 'py-10 px-4')}>
+    <main
+      className={clsx('overflow-hidden', 'flex flex-col', 'w-full h-screen')}
+    >
       {/* Head */}
-      <div className="flex justify-between items-center gap-4 max-[1080px]:flex-wrap">
-        <div className="flex-1">
-          {isUserPending ? (
-            <SkeletonLoading className="w-1/4 h-4" />
-          ) : (
-            <div
-              className={clsx(
-                'font-hanken-grotesk',
-                'text-[#C7C4D7] text-[16px] leading-tight',
-                'whitespace-nowrap',
-                'flex justify-start items-center gap-2',
-              )}
-            >
-              <Link href="/projects">Teams</Link>
-              <IconAngleRight />
-              {user ? <span>{userFullName}</span> : <i>Untitled</i>}
-            </div>
-          )}
-          <div
-            className={clsx('flex justify-start items-center gap-3', 'mt-1')}
-          >
-            {isUserPending || !user ? (
-              <SkeletonLoading className="w-1/3 h-7.5" />
-            ) : (
-              <>
-                <h1
-                  className={clsx(
-                    'font-hanken-grotesk font-bold',
-                    'text-[#DAE2FD] text-[24px] leading-tight',
-                  )}
-                >
-                  {user ? userFullName : <i>Untitled</i>}
-                </h1>
-              </>
-            )}
-          </div>
-        </div>
-      </div>
+      <AppShellHead
+        breadcrumb={{ label: userFullName, href: `/teams/${user?.id}` }}
+      />
       {/* Body */}
-      <div className="mt-10">
+      <div className={clsx('overflow-y-auto', 'flex-1 min-h-0', 'py-4 px-6')}>
         <div className="flex justify-start items-center gap-4 w-full">
           {/* Profile Image */}
           <div
@@ -184,74 +163,108 @@ export default function TeamItemPage() {
                 <div className="font-hanken-grotesk text-forground">
                   Projects
                 </div>
+                <SingleSelect
+                  classNames={{
+                    root: 'max-w-50 ml-auto',
+                    trigger: 'h-10!',
+                  }}
+                  value={{
+                    id: myProjectType.toLowerCase(),
+                    label: myProjectType,
+                    value: myProjectType,
+                  }}
+                  options={[
+                    {
+                      id: 'owned',
+                      label: 'Owned Projects',
+                      value: 'Owned',
+                    },
+                    {
+                      id: 'assigned',
+                      label: 'Assigned Projects',
+                      value: 'Assigned',
+                    },
+                  ]}
+                  onChange={(selected) => {
+                    setMyProjectType(selected.value as 'Owned' | 'Assigned');
+                  }}
+                />
               </div>
-              <div className="flex flex-col gap-6">
-                {user?.memberProjects?.map((projectItem, projectIndex) => {
-                  const startEndDate =
-                    momentTimezone(projectItem.startDate)
-                      .tz(timezone)
-                      .format('MMM DD, YYYY') +
-                    ' - ' +
-                    momentTimezone(projectItem.endDate)
-                      .tz(timezone)
-                      .format('MMM DD, YYYY');
+              {isUserPending ? (
+                <SkeletonLoading className="w-full h-20" />
+              ) : visibleProjects.length > 0 ? (
+                <div className="flex flex-col gap-6">
+                  {visibleProjects.map((projectItem, projectIndex) => {
+                    const startEndDate =
+                      momentTimezone(projectItem.startDate)
+                        .tz(timezone)
+                        .format('MMM DD, YYYY') +
+                      ' - ' +
+                      momentTimezone(projectItem.endDate)
+                        .tz(timezone)
+                        .format('MMM DD, YYYY');
 
-                  const totalTasks = projectItem.tasks?.length || 0;
+                    const totalTasks = projectItem.tasks?.length || 0;
 
-                  const totalCompletedTasks =
-                    projectItem.tasks?.filter((item) => item.status === 'DONE')
-                      .length || 0;
+                    const totalCompletedTasks =
+                      projectItem.tasks?.filter(
+                        (item) => item.status === 'DONE',
+                      ).length || 0;
 
-                  return (
-                    <Link
-                      className={clsx(
-                        'overflow-hidden',
-                        'min-h-32 h-auto',
-                        'p-6',
-                        'rounded-lg',
-                        'bg-[#0B1326]',
-                        'border border-l-[6px] border-transparent hover:border-[#464554]',
-                      )}
-                      key={projectItem.id}
-                      style={{
-                        borderLeftColor: projectItem.primaryColor,
-                      }}
-                      href={`/projects/${projectItem.id}`}
-                    >
-                      <div className="flex justify-end items-center gap-4">
-                        <div className="mr-auto">
-                          <ProjectTitleUI title={projectItem.title} />
-                        </div>
-                        <ProjectUrgencyUI urgency={projectItem.urgency} />
-                        <ProjectStatusUI status={projectItem.status} />
-                      </div>
-                      <div className="w-full mt-3">
-                        <ProjectDescriptionUI
-                          description={projectItem.description ?? ''}
-                        />
-                      </div>
-                      <div
+                    return (
+                      <Link
                         className={clsx(
-                          'flex justify-start items-center gap-4',
-                          'mt-4',
+                          'overflow-hidden',
+                          'min-h-32 h-auto',
+                          'p-6',
+                          'rounded-lg',
+                          'bg-[#0B1326]',
+                          'border border-l-[6px] border-transparent hover:border-[#464554]',
                         )}
+                        key={projectItem.id}
+                        style={{
+                          borderLeftColor:
+                            projectItem.primaryColor ?? PROJECT_DEFAULT_COLOR,
+                        }}
+                        href={`/projects/${projectItem.id}`}
                       >
-                        <ProjectTaskCounterUI
-                          current={totalCompletedTasks}
-                          total={totalTasks}
-                        />
-                        <div className="flex justify-center items-center gap-2">
-                          <IconCalendar2 />
-                          {startEndDate}
+                        <div className="flex justify-end items-center gap-4">
+                          <div className="mr-auto">
+                            <ProjectTitleUI title={projectItem.title} />
+                          </div>
+                          <ProjectStatusUI status={projectItem.status} />
+                          <ProjectPriorityUI priority={projectItem.priority} />
                         </div>
-                        <div className="ml-auto">
-                          <ProjectMembersUI members={projectItem.members} />
+                        <div className="w-full mt-3">
+                          <ProjectDescriptionUI
+                            description={projectItem.description ?? ''}
+                          />
                         </div>
-                      </div>
-                    </Link>
-                  );
-                })}
-              </div>
+                        <div
+                          className={clsx(
+                            'flex justify-start items-center gap-4',
+                            'mt-4',
+                          )}
+                        >
+                          <ProjectTaskCounterUI
+                            current={totalCompletedTasks}
+                            total={totalTasks}
+                          />
+                          <div className="flex justify-center items-center gap-2">
+                            <IconCalendar2 />
+                            {startEndDate}
+                          </div>
+                          <div className="ml-auto">
+                            <ProjectMembersUI members={projectItem.members} />
+                          </div>
+                        </div>
+                      </Link>
+                    );
+                  })}
+                </div>
+              ) : (
+                <div className="text-center">No Projects Found</div>
+              )}
             </div>
           </div>
           {/* Right */}
