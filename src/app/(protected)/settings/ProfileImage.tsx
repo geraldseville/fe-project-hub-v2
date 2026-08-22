71b7181487360d02c7f4e0a6fbd47adbb9753043
Gerald <58556422+geraldseville@users.noreply.github.com>
2026-08-23 06:17:15 +0800

    refactor(components): integrate ImageDropZone and UserImageCropModal into ProfileImage

--- src/app/(protected)/settings/ProfileImage.tsx
+++ src/app/(protected)/settings/ProfileImage.tsx
@@ -1,194 +1,145 @@
-import { useRef } from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 
 import clsx from 'clsx';
 
-import { useUpdateMe } from '@/hooks/mutations/useUpdateMe';
-import { useUploadUserProfileImage } from '@/hooks/mutations/useUploadProfileImage';
 import { useMe } from '@/hooks/queries/useMe';
-import { useToastStore } from '@/hooks/ui/useToastStore';
 
 import { getFullName, userInitials } from '@/utils/user.utils';
 
-import LoaderSpinner from '@/components/elements/LoaderSpinner';
+import ImageDropZone from '@/components/reusable/ImageDropZone';
+import { IconImage1 } from '@/components/svgs/icons';
+
+import UserImageCropModal from './UserImageCropModal';
 
 export default function ProfileImage() {
-  const toast = useToastStore();
-
   const { data: me } = useMe();
+
+  const [imageFile, setImageFile] = useState<File | null>(null);
 
   const userFullName = getFullName(me?.firstName, me?.lastName);
 
-  const updateMe = useUpdateMe();
-
-  const uploadUserProfileImage = useUploadUserProfileImage();
-
-  const uploadUserProfileImagePending = uploadUserProfileImage.isPending;
-
-  const userImageInputRef = useRef<HTMLInputElement>(null);
-
-  const handleUploadProfileImage = async (file: File) => {
-    if (!file) return;
-
-    try {
-      const formData = new FormData();
-
-      formData.append('image', file);
-
-      const resultProfileImage =
-        await uploadUserProfileImage.mutateAsync(formData);
-
-      if (!resultProfileImage.data?.url) {
-        throw new Error('failed to upload profile image.');
-      }
-
-      await updateMe.mutateAsync({
-        imageUrl: resultProfileImage.data.url,
-      });
-
-      toast.success('user updated profile image.');
-    } catch (err) {
-      toast.failed(
-        err instanceof Error ? err.message : 'failed to upload profile image.',
-      );
-    }
-  };
-
   return (
-    <div
-      className={clsx(
-        'group',
-        'relative',
-        'flex justify-center items-center',
-        'min-w-34 w-34 h-34',
-        'rounded-xl',
-        'border-4 border-background',
-        'bg-background',
-      )}
-    >
-      {uploadUserProfileImagePending ? (
-        <div
-          className={clsx(
-            'absolute z-3 inset-0',
-            'flex justify-center items-center',
-            'w-full h-full',
+    <>
+      <div
+        className={clsx(
+          'group',
+          'relative overflow-hidden',
+          'flex justify-center items-center',
+          'min-w-34 w-34 h-34',
+          'rounded-xl',
+          'border-4 border-background',
+          'bg-background',
+        )}
+      >
+        <ImageDropZone
+          onFileSelect={setImageFile}
+          renderChild={(isDragging) => (
+            <>
+              {me?.imageUrl ? (
+                <>
+                  <Image
+                    className={clsx(
+                      'relative z-10',
+                      'w-full h-full object-cover object-top',
+                      'rounded-xl',
+                    )}
+                    src={me.imageUrl}
+                    alt={userFullName}
+                    title={userFullName}
+                    width={136}
+                    height={136}
+                    draggable={false}
+                    loading="eager"
+                  />
+
+                  <div
+                    className={clsx(
+                      'absolute inset-0 z-20',
+                      'flex justify-center items-center',
+                      'w-full h-full',
+                      'rounded-[inherit]',
+                      'bg-black/60',
+                      'transition-all duration-200',
+                      'opacity-0 group-hover:opacity-100',
+                    )}
+                  >
+                    <div
+                      className={clsx(
+                        'font-inter',
+                        'text-[10px] uppercase',
+                        'p-2',
+                        'rounded-md',
+                        'bg-black',
+                      )}
+                    >
+                      Change
+                    </div>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div
+                    className={clsx(
+                      'relative z-10',
+                      'flex justify-center items-center',
+                      'w-full h-full',
+                      'text-[20px]',
+                    )}
+                  >
+                    {userInitials(userFullName ?? '')}
+                  </div>
+
+                  <div
+                    className={clsx(
+                      'absolute z-20 inset-0',
+                      'flex justify-center items-center',
+                      'rounded-[inherit]',
+                      'bg-black/10',
+                      'transition-all duration-200',
+                      'opacity-0 group-hover:opacity-100',
+                    )}
+                  >
+                    <div
+                      className={clsx(
+                        'font-inter',
+                        'text-[10px] uppercase',
+                        'p-2',
+                        'rounded-md',
+                        'bg-black',
+                      )}
+                    >
+                      Upload
+                    </div>
+                  </div>
+                </>
+              )}
+
+              {/* Dragging overlay */}
+              {isDragging && (
+                <div
+                  className={clsx(
+                    'absolute z-30 inset-0',
+                    'flex justify-center items-center',
+                    'rounded-[inherit]',
+                    'bg-black/70',
+                    'border-2 border-dashed border-primary',
+                  )}
+                >
+                  <IconImage1 className="min-w-5 w-5 h-5" />
+                </div>
+              )}
+            </>
           )}
-        >
-          <LoaderSpinner className={clsx('min-w-6! size-6!')} />
-        </div>
-      ) : me?.imageUrl ? (
-        <>
-          <Image
-            className={clsx(
-              'relative z-1',
-              'w-full h-full object-cover object-top',
-              'rounded-xl',
-            )}
-            src={me.imageUrl}
-            alt={userFullName}
-            title={userFullName}
-            width={136}
-            height={136}
-            draggable={false}
-            loading="eager"
-          />
-          <div
-            className={clsx(
-              'absolute inset-0 z-2',
-              'w-full h-full',
-              'rounded-[inherit]',
-              'bg-black/60',
-              'transition-all duration-200',
-              'opacity-0 group-hover:opacity-100',
-            )}
-          >
-            <label
-              className={clsx(
-                'flex justify-center items-center',
-                'w-full h-full',
-                'cursor-pointer',
-              )}
-              htmlFor="profileImage"
-            >
-              <div
-                className={clsx(
-                  'font-inter',
-                  'text-[10px] uppercase',
-                  'p-2',
-                  'rounded-md',
-                  'bg-black',
-                )}
-              >
-                Change
-              </div>
-            </label>
-            <input
-              className="hidden"
-              id="profileImage"
-              ref={userImageInputRef}
-              type="file"
-              accept="image/*"
-              onChange={(e) => {
-                const file = e.target.files?.[0];
-
-                if (file) {
-                  handleUploadProfileImage(file);
-                }
-              }}
-            />
-          </div>
-        </>
-      ) : (
-        <>
-          <div className={clsx('relative', 'text-[20px]')}>
-            {userInitials(userFullName ?? '')}
-          </div>
-          <div
-            className={clsx(
-              'absolute inset-0',
-              'rounded-[inherit]',
-              'bg-black/10',
-              'transition-all duration-200',
-              'opacity-0 group-hover:opacity-100',
-            )}
-          >
-            <label
-              className={clsx(
-                'flex justify-center items-center',
-                'w-full h-full',
-                'cursor-pointer',
-              )}
-              htmlFor="profileImage"
-            >
-              <div
-                className={clsx(
-                  'font-inter',
-                  'text-[10px] uppercase',
-                  'p-2',
-                  'rounded-md',
-                  'bg-black',
-                )}
-              >
-                Upload
-              </div>
-            </label>
-            <input
-              className="hidden"
-              id="profileImage"
-              ref={userImageInputRef}
-              type="file"
-              accept="image/*"
-              onChange={(e) => {
-                const file = e.target.files?.[0];
-
-                if (file) {
-                  handleUploadProfileImage(file);
-                }
-              }}
-            />
-          </div>
-        </>
-      )}
-    </div>
+        />
+      </div>
+      <UserImageCropModal
+        isOpen={imageFile ? true : false}
+        onClose={() => {
+          setImageFile(null);
+        }}
+        imageFile={imageFile}
+      />
+    </>
   );
 }
