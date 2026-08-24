84a77578a89f5f211e460c05f79bab6b442a8afe
Gerald <58556422+geraldseville@users.noreply.github.com>
2026-08-24 18:58:44 +0800

    refactor(useUploadUserProfileImage): remove unused queryClient instance and imports

--- src/hooks/mutations/useUploadProfileImage.ts
+++ src/hooks/mutations/useUploadProfileImage.ts
@@ -1,17 +1,9 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 
 import { uploadUserProfileImage } from '@/api/image.api';
 
 export function useUploadUserProfileImage() {
-  const queryClient = useQueryClient();
-
   return useMutation({
     mutationFn: uploadUserProfileImage,
-
-    onSuccess() {
-      queryClient.invalidateQueries({
-        queryKey: ['me'],
-      });
-    },
   });
 }
