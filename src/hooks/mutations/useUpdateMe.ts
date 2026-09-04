422205729f65ecb1d0b45ce0a63a58eec97e8b0a
Gerald <58556422+geraldseville@users.noreply.github.com>
2026-09-04 13:53:34 +0800

    feat(hook): implement optimistic updates and add users query invalidation to useUpdateMe mutation

--- src/hooks/mutations/useUpdateMe.ts
+++ src/hooks/mutations/useUpdateMe.ts
@@ -2,25 +2,43 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { updateMe } from '@/api/user.api';
 
+import type { User } from '@/types/user.types';
+
 export function useUpdateMe() {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: updateMe,
 
-    async onSuccess(response) {
+    async onMutate(updatedUser) {
+      await queryClient.cancelQueries({
+        queryKey: ['me'],
+      });
+
+      const previousUser = queryClient.getQueryData(['me']);
+
+      queryClient.setQueryData(['me'], (currentUser: User) => {
+        if (!currentUser) return currentUser;
+
+        return {
+          ...currentUser,
+          ...updatedUser,
+        };
+      });
+
+      return { previousUser };
+    },
+
+    onError(_error, _updatedUser, context) {
+      if (context?.previousUser) {
+        queryClient.setQueryData(['me'], context.previousUser);
+      }
+    },
+
+    onSuccess(response) {
       if (response.data?.user) {
         queryClient.setQueryData(['me'], response.data.user);
       }
-
-      await Promise.all([
-        queryClient.invalidateQueries({
-          queryKey: ['me'],
-        }),
-        queryClient.invalidateQueries({
-          queryKey: ['users'],
-        }),
-      ]);
     },
   });
 }
