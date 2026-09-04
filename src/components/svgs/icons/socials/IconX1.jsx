d822071ee84592a27e9322dbd7a0349158beaeeb
Gerald <58556422+geraldseville@users.noreply.github.com>
2026-09-04 13:52:55 +0800

    feat(svgs): add new svg icons as resuable components

---
+++ src/components/svgs/icons/socials/IconX1.jsx
@@ -0,0 +1,18 @@
+export default function IconX1({ className = '' }) {
+  return (
+    <svg
+      className={className}
+      width="12"
+      height="11"
+      viewBox="0 0 12 11"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        className="path-01"
+        d="M9.45 0H11.2903L7.27029 4.65905L12 11H8.29714L5.39486 7.15503L2.07771 11H0.235714L4.53514 6.01497L0 0.00086695H3.79714L6.41657 3.51466L9.45 0ZM8.80286 9.88335H9.82286L3.24 1.05856H2.14629L8.80286 9.88335Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
